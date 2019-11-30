bbecc50247e515abbf866b669c809d669401be81
maphnew <zcm3118@gmail.com>
2019-11-30 19:12:53 +0900

    add High Low temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " +body.currently.temperature+ " degrees out. There is a " +body.currently.precipProbability+"% chance of rain.")
+            // console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + "It is currently " +body.currently.temperature+ " degrees out. There is a " +body.currently.precipProbability+"% chance of rain. \n \
+            Temperature High: " + body.daily.data[0].temperatureHigh + " Temperature Low: " + body.daily.data[0].temperatureLow)
         }
     })
 }
